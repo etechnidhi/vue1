16691fd735e94cf8a62db41b8d0c49203fc255c7
etechnidhi <nidhi_sharma@excellencetechnologies.in>
2018-07-17 12:26:46 +0530

    2

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,22 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
+
     <!-- <img src="./assets/logo.png"> -->
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-=======
-    <img src="./assets/logo.png">
-    <!-- <List /> -->
-    <Control />
->>>>>>> 73bfcdc2f6d907a0d0572cd2ce6fa906efec0a1c
+    <HelloWorld />
+<!-- <HelloWorld/> -->
+    
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
+
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
     HelloWorld
-=======
-// import List from './components/List.vue';
-import Control from './components/Control.vue';
-export default {
-  name: 'app',
-  components: {
-    // List
-    Control
->>>>>>> 73bfcdc2f6d907a0d0572cd2ce6fa906efec0a1c
+
   }
 }
 </script>
